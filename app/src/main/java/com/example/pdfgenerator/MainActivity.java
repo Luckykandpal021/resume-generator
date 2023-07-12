03a35afd58228be1a1448707197d787f4a7a64cc
Luckykandpal021 <luckykandpal059@gmail.com>
2023-07-12 13:02:46 +0530

    image upload

--- app/src/main/java/com/example/pdfgenerator/MainActivity.java
+++ app/src/main/java/com/example/pdfgenerator/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.pdfgenerator;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.os.Bundle;
 import android.graphics.pdf.PdfDocument;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -17,65 +24,23 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    Button generatePdfButton;
-    EditText editText;
+    private ActivityResultLauncher<String> mTakePhoto;
+    private ImageView imgGallery;
+    private Button btnGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = findViewById(R.id.inputValue1);
-        generatePdfButton = findViewById(R.id.create);
-        generatePdfButton.setOnClickListener(V -> {
-            generatePdf(editText.getText().toString());
+        imgGallery = findViewById(R.id.imgGallery);
+        btnGallery = findViewById(R.id.btnGallery);
+        mTakePhoto = registerForActivityResult(new ActivityResultContracts.GetContent(), result -> imgGallery.setImageURI(result));
+        btnGallery.setOnClickListener(V -> {
+            mTakePhoto.launch("image/*");
         });
 
-    }
-
-    private void generatePdf(String inputVal) {
-        PdfDocument document = new PdfDocument();
-        int pdfWidth = getResources().getInteger(R.integer.pdf_width);
-        int pdfHeight = getResources().getInteger(R.integer.pdf_height);
-
-        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(pdfWidth, pdfHeight, 1).create();
-
-        PdfDocument.Page page = document.startPage(pageInfo);
-        Canvas canvas = page.getCanvas();
-
-        Paint paint = new Paint();
-        paint.setColor(Color.BLACK);
-        paint.setTextSize(150f);
-        float x = 150f;
-        float y = 250f;
-        canvas.drawText(inputVal, x, y, paint);
-
-
-        document.finishPage(page);
-
-        File file = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DOWNLOADS), "/" + "hello.pdf");
-
-        try {
-            document.writeTo(new FileOutputStream(file));
-            // PDF generation successful
-            showToast("PDF generated successfully!");
-        } catch (IOException e) {
-            e.printStackTrace();
-            // PDF generation failed
-            showToast("Error generating PDF!" + e);
-
-        } finally {            // Close the document
-            document.close();
-
-
-        }
-
 
     }
 
-
-    private void showToast(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-    }
 
 }
